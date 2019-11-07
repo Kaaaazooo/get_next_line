da729dcfaed117fcdbe5ebccb3e5d15c363dbf42
Sami Brugier <sabrugie@e-r10-p5.s19.be>
2019-11-07 16:18:14 +0100

    secured if -1 && all tests OK

---
+++ get_next_line_bonus.h
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sabrugie <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/11/01 11:19:14 by sabrugie          #+#    #+#             */
+/*   Updated: 2019/11/03 14:17:46 by sabrugie         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <limits.h>
+# include <fcntl.h>
+# include <stdio.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 999
+# endif
+
+int			get_next_line(int fd, char **line);
+void		*ft_memcpy(void *dst, const void *src, size_t n);
+int			has_nl(char *str);
+char		*ft_cut(char *s1);
+char		*ft_join(char *s1, char *s2);
+char		*to_next(int fd, char **remain, char *buf, int ret);
+
+#endif
